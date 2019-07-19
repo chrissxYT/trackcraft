a2d4d2b8e586fc53195830345ed921904e03ab5a
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-07-19 19:36:09 +0200

    simplified cat_urls.sh

--- cat_urls.sh
+++ cat_urls.sh
@@ -7,7 +7,7 @@ echo -n "($I/$COUNT)" >&2
 
 for u in $(cat src_lists) ; do
         echo -n "\r($I/$COUNT)" >&2
-        curl -L $u 2> /dev/null
+        curl -fsSL $u
         I=$(($I + 1))
 done > blocklist
 echo "" >&2
