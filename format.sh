2809f8a2e05bb39cc3c251df5c133927064c0654
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-09-15 10:56:58 +0200

    fixed the whitelisting

--- format.sh
+++ format.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
+sed 's/\t//g' |
+sed 's/ //g' |
 sed 's/\r/\n/g' |
 sed 's/\n\n/\n/g' |
 sed 's/0\.0\.0\.0//g' |
 sed 's/127\.0\.0\.1//g' |
-sed 's/\t//g' |
-sed 's/ //g' |
-sed 's/^#.*\n//g' |
-sed 's/#.*//g' |
+sed 's/^#.*$//' |
+sed 's/#.*$//' |
 sed 's/^www\.//' |
-sed 's/.*spotify\.com.*\n//g' |
-sed 's/.*ipinfo\.io.*\n//g' |
-sed 's/s\.youtube\.com.*\n//g' |
-sed 's/s3\.amazonaws\.com.*\n//g' |
-sed 's/amzn\.to.*\n//g' |
-sed 's/^storage\.googleapis\.com.*\n//g' |
+sed 's/^.*spotify\.com.*$//' |
+sed 's/^ipinfo\.io.*$//' |
+sed 's/^s\.youtube\.com.*$//' |
+sed 's/^s3\.amazonaws\.com.*$//' |
+sed 's/^amzn\.to.*$//' |
+sed 's/^storage\.googleapis\.com//' |
 sort |
 uniq |
 tail -n +2
